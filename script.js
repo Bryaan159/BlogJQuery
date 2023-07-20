5c8420009a6ddac5a5ce3947f00eba58777cc90b
Bryaan159 <00057720@uca.edu.sv>
2023-07-20 14:53:22 -0600

    Se hizo las cartas de los post de cada informacion

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
 // script.js
 
-// Utilizar jQuery para detectar cuándo la página se ha cargado completamente
-$(document).ready(function() {
-    // Este código se ejecutará cuando la página se haya cargado completamente
-
-    // Ejemplo de mostrar un mensaje en una ventana emergente (alert) cuando se haga clic en un botón
-    $('#myButton').click(function() {
-        alert('¡Hola desde jQuery!');
-    });
-});
